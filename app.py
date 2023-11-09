942e5b0d31a2cfd1d14be35bb5df20d2fb1c6da5
Taher Ali badnawarwala <tahercoolguy@gmail.com>
2023-11-09 11:44:44 +0530

    changed

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.title('📝 Article Generator App')
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 chat_model = None
 if openai_api_key.startswith('sk-'):
-    chat_model = ChatOpenAI(model_name='gpt-4-32k', openai_api_key="ssss")
+    chat_model = ChatOpenAI(model_name='gpt-3.5-turbo-16k', openai_api_key="ssss")
 else:
     st.warning('Please enter a valid OpenAI API key!', icon='⚠')
 
